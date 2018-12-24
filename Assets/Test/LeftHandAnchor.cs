0a16c10edeb379c4c678f25c790ce31045c75cb7
長島光太 <se.kokuto@gmail.com>
2018-12-24 23:50:09 +0900

    first

---
+++ Assets/Test/LeftHandAnchor.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeftHandAnchor : MonoBehaviour {
+
+    public BulletMove bulletGenelate;
+
+    private bool t = false;
+
+	void Start () {
+		
+	}
+	
+	
+
+	void Update () {
+        if (OVRInput.GetDown(OVRInput.RawButton.LIndexTrigger))
+        {
+            Debug.Log("左人差し指トリガーを押した");
+            if (t == false)
+            {
+                t = true;
+            }
+            else
+            {
+                t = false;
+            }
+        }
+
+        if (t)
+        {
+            bulletGenelate.Genelat();
+        }
+    }
+}
---
+++ Assets/Test/RightHandAnchor.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightHandAnchor : MonoBehaviour {
+
+    public BulletMove bulletGenelate;
+    private bool t = false;
+
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        
+        if (OVRInput.GetDown(OVRInput.RawButton.RIndexTrigger))
+        {
+            Debug.Log("右人差し指トリガーを押した");
+            if (t == false)
+            {
+                t = true;
+            }
+            else
+            {
+                t = false;
+            }
+        }
+
+        if (t)
+        {
+            bulletGenelate.Genelat();
+        }
+
+    }
+}
